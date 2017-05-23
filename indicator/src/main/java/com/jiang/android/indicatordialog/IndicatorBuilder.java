d6d651fc2ff1b50e57f389aaec888a968347b9a3
jyuesong <jyuesong@gmail.com>
2017-05-23 09:29:42 +0800

    add animator support

--- indicator/src/main/java/com/jiang/android/indicatordialog/IndicatorBuilder.java
+++ indicator/src/main/java/com/jiang/android/indicatordialog/IndicatorBuilder.java
@@ -124,7 +124,7 @@ public class IndicatorBuilder {
         return IndicatorDialog.newInstance(mContext, this);
     }
 
-    @IntDef({TOP, BOTTOM, LEFT, RIGHT})
+    @IntDef({TOP, BOTTOM})
     @Retention(RetentionPolicy.SOURCE)
     public @interface ARROWDIRECTION {
     }
